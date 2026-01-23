b3504e1d20b21b047b1c8455e6321c5c5c274be8
Muhammad Faruq Arkaan <112605831+Arkaan16@users.noreply.github.com>
2026-01-23 07:42:36 +0700

    employee assets details (#8)

--- resources/views/components/employee-sidebar.blade.php
+++ resources/views/components/employee-sidebar.blade.php
@@ -53,11 +53,13 @@
             </a>
 
             {{-- ASET --}}
-            {{-- Saya menghapus wire:navigate sementara karena href="#" agar tidak error --}}
-            <a href="#" 
+            <a href="{{ route('employee.assets.index') }}" wire:navigate
                @click="sessionStorage.setItem('sidebarScrollPos', $el.closest('.overflow-y-auto').scrollTop)"
-               class="flex items-center space-x-3 px-4 py-3 rounded-xl transition-all duration-200 group text-gray-600 hover:text-gray-900 hover:bg-gray-50">
-                <svg class="w-5 h-5 text-gray-400 group-hover:text-gray-500" 
+               class="flex items-center space-x-3 px-4 py-3 rounded-xl transition-all duration-200 group"
+               :class="window.location.href.includes('assets') ? 'bg-gray-100 text-indigo-600 font-semibold' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50'">
+               
+                <svg class="w-5 h-5 group-hover:text-gray-500" 
+                     :class="window.location.href.includes('assets') ? 'text-indigo-600' : 'text-gray-400'"
                      fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                 </svg>
